86046251ed86e14581c0c6f105bb68764ac40850
Siddharth Chandra <chandra_s@India.XoriantCorp.com>
2016-04-07 19:03:09 +0530

    Fixed File Scanner Extension Length

--- cordova-file-angular.js
+++ cordova-file-angular.js
@@ -1,14 +1,27 @@
 var file  =  angular.module("CordovaFileReader",[]);
 
-file.service("cordovaFile",function()
+file.service("cordovaFile",function($q,$timeout)
 {
-  var globalList = [];
-  var currentlist = [];
+
+  var list = [];
+
+  //var deferred = $q.defer();
+ // var promises = [deferred.promise];
   return {
 
     scan : function(url,fileType,success,failure)
     {
-     
+      var i=0;
+      while(i!=fileType.length)
+      {
+        list[fileType[i]] = [];
+        i+=1;
+      }
+       // list[fileType[0]] = [];
+       // list[fileType[1]] = [];
+       //Get List Of FilesType and Create named associativr Array
+
+
       //Function Definition For fileEntry and Error
       var fileSearch = function (entry) {
    
@@ -28,43 +41,68 @@ file.service("cordovaFile",function()
                   } else 
                   {
         
-                    if(entries[i].name.indexOf(fileType) != -1)
+                    var j=0;
+                    while(j!=fileType.length)
                     {
+
+                      if(entries[i].name.indexOf(fileType[j]) != -1)
+                      {
                       
                       var file = {};
-                      file.name = entries[i].name;
-                      file.nativeURL = entries[i].nativeURL;
-                      file.fullPath = entries[i].fullPath;
-                      currentlist.push(file);
-                      globalList.push(file);
+                      file = entries[i];
+                      file.type = fileType[j];
+                      
+                     list[fileType[j]].push(file);
+
+                      }
+                    j+=1;
                     }
+
+                 //    if(entries[i].name.indexOf(fileType[0]) != -1)
+                 //    {
+                      
+                 //      var file = {};
+                 //      file = entries[i];
+                 //      file.type = fileType[0];
+                      
+                 //     list[fileType[0]].push(file);
+                 //   //   promises.push(file);
+                 // //     console.log(file);
+                 //    }
+                 //    else if(entries[i].name.indexOf(fileType[1]) != -1)
+                 //    {
+                      
+                 //      var file = {};
+                 //      file = entries[i];
+                 //      file.type = fileType[1];
+                 //      list[fileType[1]].push(file);
+                 //    //  console.log(file);
+                 //     //    promises.push(file);
+                 //    }
+
+
                   }
                   i+=1;
               }
 
-             // Return List As CurrentSearch File and ll Files Searched in globalList
-             var fileHolder = {};
-             fileHolder.currentList = currentlist;
-             //fileHolder.currentList.push(currentlist);
+             // Return List As CurrentSearch File and ll Files Searched in globalLis
+             // deferred.resolve();
 
-             fileHolder.globalList = globalList;
-            // fileHolder.globalList.push(globalList);
-
-             success(fileHolder);  
+              
+           success(list);  
           },
           function (error) 
           {
               console.log(error);
-              failure(error);
+            //  deferred.reject(list);
+            failure(error);
+
           });
+
+      //  return $q.all(promises);
     };   //Func ENds
 
-    var fileError = function (error) {
-      console.log(error);
-      failure(error);
-     
-    };
-
+   
      var i=0;
      while(i!=url.length) 
      {
@@ -72,7 +110,11 @@ file.service("cordovaFile",function()
         {
             continue; // skip blank / non-existent paths for this platform
         }
-        window.resolveLocalFileSystemURL(url[i],fileSearch, fileError);
+        window.resolveLocalFileSystemURL(url[i],fileSearch, function (error) {
+          console.log(error);
+          failure(error);
+        });
+
         i+=1;
       }
 
@@ -82,3 +124,10 @@ file.service("cordovaFile",function()
 
 });
 
+
+/*
+file.name = entries[i].name;
+                      file.nativeURL = entries[i].nativeURL;
+                      file.fullPath = entries[i].fullPath;
+
+*/
